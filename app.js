02ac8d90bc3fbf721aa014df012f6fcefa750736
Ivan Kolesnev <i.v.kolesnev@mail.ru>
2021-03-05 23:00:09 +0300

    implement: open and close description people

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ async function getData() {
     const data = await Promise.all(responses.map((res) => res.json()));
 
     data.forEach((person) => arrayPeople.push(...person.results));
+    console.log(arrayPeople);
 
     displayPeoplePage(arrayPeople, numberOfArticlesOnPage);
     paginationPeople(arrayPeople, numberOfArticlesOnPage);
@@ -35,8 +36,9 @@ function createArrayUrlsOfPeople() {
   return people;
 }
 
-function createId() {
-  return new Date().getTime();
+function createId(id) {
+  let arr = id.split("");
+  return arr.slice(-7, -1).join("");
 }
 
 function displayPeoplePage(arrayPeople, numberOfArticlesOnPage) {
@@ -49,20 +51,63 @@ function displayPeoplePage(arrayPeople, numberOfArticlesOnPage) {
   let paginatedArray = arrayPeople.slice(start, end);
 
   paginatedArray.map((person) => {
-    output += `<div class="people__content">
-                    <h3 class="people__name">${person.name}</h3>
-                    <div class="people__description">
-                      <p class="people__gender"> <span class="people__subtitle">Gender:</span> ${person.gender}</p>
-                      <p class="people__birth-year"> <span class="people__subtitle">Birthday:</span> ${person.birth_year}</p>
-                      <p class="people__height"> <span class="people__subtitle">Height:</span> ${person.height}</p>
-                      <p class="people__mass"> <span class="people__subtitle">Mass:</span> ${person.mass}</p>
-                      <p class="people__eye-color"> <span class="people__subtitle">Eye color:</span> ${person.eye_color}</p>
-                      <p class="people__hair-color"> <span class="people__subtitle">Hair color:</span> ${person.hair_color}</p>
-                      <p class="people__skin-color"> <span class="people__subtitle">Skin color:</span> ${person.skin_color}</p>
+    output += `<div class="people__content" id=${createId(person.created)}>
+                    <h3 class="people__name" id=${createId(person.created)}>${
+      person.name
+    }</h3>
+                    <div class="people__description" id=${createId(
+                      person.created
+                    )}>
+                      <p class="people__gender"> <span class="people__subtitle">Gender:</span> ${
+                        person.gender
+                      }</p>
+                      <p class="people__birth-year"> <span class="people__subtitle">Birthday:</span> ${
+                        person.birth_year
+                      }</p>
+                      <p class="people__height"> <span class="people__subtitle">Height:</span> ${
+                        person.height
+                      }</p>
+                      <p class="people__mass"> <span class="people__subtitle">Mass:</span> ${
+                        person.mass
+                      }</p>
+                      <p class="people__eye-color"> <span class="people__subtitle">Eye color:</span> ${
+                        person.eye_color
+                      }</p>
+                      <p class="people__hair-color"> <span class="people__subtitle">Hair color:</span> ${
+                        person.hair_color
+                      }</p>
+                      <p class="people__skin-color"> <span class="people__subtitle">Skin color:</span> ${
+                        person.skin_color
+                      }</p>
+                      <button class="people__button people__button--absolute" id=${createId(
+                        person.created
+                      )}>X</button>
                     </div>
                 </div>`;
 
     wrapperPeopleNode.innerHTML = output;
+  });
+
+  wrapperPeopleNode.addEventListener("click", function (e) {
+    if (e.target.classList.contains("people__name")) {
+      let peopleContentNode = e.target.parentNode;
+      let peopleDescriptionNode = peopleContentNode.querySelector(
+        ".people__description"
+      );
+      let btnCloseDescriptionNode = peopleContentNode.querySelector(
+        ".people__button--absolute"
+      );
+
+      if (e.target) {
+        if (peopleContentNode.dataset.id === peopleDescriptionNode.dataset.id) {
+          peopleDescriptionNode.style.display = "flex";
+        }
+
+        btnCloseDescriptionNode.addEventListener("click", () => {
+          peopleDescriptionNode.style.display = "none";
+        });
+      }
+    }
   });
 }
 
